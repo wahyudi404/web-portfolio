0e30ccbe0e66a658958d47a8f9a88eeb974daf1b
Wahyudi <74516756+wahyudi404@users.noreply.github.com>
2024-09-06 18:48:12 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-    container: {
-      padding: '4rem',
-    },
   },
   plugins: [require("tailgrids/plugin")],
 }
