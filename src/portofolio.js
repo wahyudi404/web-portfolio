5a1eec12ff7543a8449d5fab6fb64b9e9c93b6f0
Wahyudi <wahyuyudi2801@gmail.com>
2026-05-01 18:02:20 +0700

    update portofolio

---
+++ src/portofolio.js
@@ -0,0 +1,144 @@
+const data = [
+    {
+        id: 1,
+        title: "QWords",
+        thumbnail: "./img/portofolio/qwords.png",
+        github: "https://github.com/wahyuyudi2801/task5-qwords",
+        website: "https://www.figma.com/design/m7BdGusw3PeCYhSgBIAOjk/Qwords?m=dev&t=NAi6S1gViBsxOH4S-1",
+        description: "Website landing page untuk QWords, sebuah perusahaan penyedia layanan hosting dan domain.",
+        summary: `
+                <p>QWords merupakan proyek magang yang saya jalani secara mandiri untuk menambah pengalaman di dunia kerja. Dalam proyek ini, saya mempelajari framework PHP yaitu Laravel selama kurang lebih satu bulan, dengan versi yang digunakan adalah Laravel 10. Proyek ini bertujuan untuk memberikan solusi digital yang inovatif bagi klien.</p>
+                <br/>
+                <p>Saya bertanggung jawab untuk merancang dan mengembangkan website landing page yang menarik, responsif, dan mudah digunakan. Dalam proses pengembangannya, saya menggunakan Laravel, Blade, dan Tailwind CSS untuk menciptakan tampilan yang modern serta user-friendly. Hasil dari proyek ini adalah sebuah landing page yang mampu menarik perhatian pengunjung dan berpotensi meningkatkan konversi. Selain itu, saya juga mendapatkan banyak pengalaman berharga, terutama dalam pengembangan tampilan web serta memahami kebutuhan pengguna. Ke depannya, saya berharap dapat terus mengembangkan kemampuan saya di bidang web development.</p>
+                `
+    },
+    {
+        id: 2,
+        title: "Metamata",
+        thumbnail: "./img/portofolio/metamata/Homepage.png",
+        github: "#",
+        website: "https://www.metamata.id/",
+        description: "Metamata adalah Platform ekosistem event di Indonesia yang menggabungkan konsep bisnis kreatif dengan gerakan sosial.",
+        summary: `
+                <p>Metamata merupakan proyek magang yang saya jalani secara mandiri untuk menambah pengalaman di dunia kerja. Dalam proyek ini, saya mempelajari framework PHP yaitu Laravel selama 3 bulan, dengan versi yang digunakan adalah Laravel 10.</p>
+                <br/>
+                <p>Saya bertanggung jawab untuk merancang dan mengembangkan halaman pengelolaan event. Dalam proses pengembangannya, saya menggunakan Laravel, React, Inertia dan Tailwind CSS. Hasil dari proyek ini adalah sebuah platform yang digunakan untuk mengelola event, seperti menjual tiket untuk acara komunitas dan festival. Selain itu, saya juga mendapatkan banyak pengalaman berharga, terutama dalam pengembangan fitur yang kompleks serta memahami kebutuhan pengguna. Ke depannya, saya berharap dapat terus mengembangkan kemampuan saya di bidang web development. Berikut merupakan beberapa gambar dari proyek ini:</p>
+                <br/>
+                <ul>
+                    <li class="font-semibold">
+                        Dashboard untuk mengelola event <br/> 
+                        <img src="./img/portofolio/metamata/Dashboard.png" alt="Dashboard" class="w-full max-w-[800px] h-auto mb-4 rounded"/> 
+                        <img src="./img/portofolio/metamata/Dashboard 2.png" alt="Dashboard 2" class="w-full max-w-[800px] h-auto mb-4 rounded"/><br/>
+                    </li>
+                    <li class="font-semibold">
+                        Halaman untuk membuat event baru
+                        <br/>
+                        <img src="./img/portofolio/metamata/create event.png" alt="Create Event" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                        <img src="./img/portofolio/metamata/create event 2.png" alt="Create Event 2" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                    </li>
+                </ul>
+                `
+    },
+    {
+        id: 3,
+        title: "Sistem Administrasi Tanah/Pertanahan",
+        thumbnail: "./img/portofolio/tanah.png",
+        github: "#",
+        website: "https://www.geoadipersada.co.id/",
+        description: "Aplikasi Administrasi Tanah/Pertanahan adalah sebuah aplikasi yang digunakan untuk mengelola data administrasi tanah dan pertanahan.",
+        summary: `
+                <p>Sistem Administrasi Tanah/Pertanahan adalah sebuah aplikasi yang digunakan untuk mengelola data administrasi tanah dan pertanahan. Projek ini dibuat untuk memenuhi kebutuhan PT Geo Adi Persada. Projek ini dibuat oleh tim yang terdiri dari 2 developer.</p>
+                <br/>
+                <p>Saya bertanggung jawab untuk merancang dan mengembangkan halaman pengelolaan data tanah. Dalam proses pengembangannya, saya menggunakan PHP Nativer, Tailwind CSS, dan MySQL sebagai database. Hasil dari proyek ini adalah sebuah aplikasi yang digunakan untuk mengelola data administrasi tanah dan pertanahan, seperti data pemilik tanah, data lokasi tanah, dan data transaksi tanah. Selain itu, saya juga mendapatkan banyak pengalaman berharga, terutama dalam pengembangan fitur yang sangat kompleks, memahami kebutuhan client, dan bekerja dalam tim.</p>
+                `
+    },
+    {
+        id: 4,
+        title: "Sistem Diagnosa Penyakit Umum Manusia",
+        thumbnail: "./img/portofolio/SPUM/hasil-diagnosa.png",
+        github: "http://github.com/wahyuyudi2801/sistem-diagnosa-penyakit",
+        website: "http://diagnosa-penyakit.screenslaver.com",
+        description: "Sistem Diagnosa Penyakit Umum Manusia adalah sebuah aplikasi yang digunakan untuk mendiagnosa penyakit umum pada manusia berdasarkan gejala yang dialami.",
+        summary: `
+                <p>Sistem Diagnosa Penyakit Umum Manusia merupakan sebuah aplikasi yang dirancang untuk membantu mengidentifikasi kemungkinan penyakit berdasarkan gejala yang dialami pengguna. Proyek ini dikembangkan sebagai bagian dari tugas akhir dan dikerjakan secara mandiri oleh satu developer.</p>
+                <br/>
+                <p>Dalam proses pengembangannya, saya bertanggung jawab penuh mulai dari tahap perancangan hingga implementasi. Teknologi yang digunakan meliputi Laravel, Inertia, React.js, Shadcn, serta MySQL. Metodologi yang diterapkan adalah Research and Development (R&D), dengan pendekatan algoritma Naive Bayes untuk mendukung proses diagnosis.</p>
+                <br/>
+                <p>Hasil dari proyek ini adalah sebuah aplikasi berbasis web yang mampu memberikan diagnosis awal terhadap penyakit umum secara sistematis berdasarkan input gejala. Selain menghasilkan produk yang fungsional, proyek ini juga memberikan pengalaman berharga dalam membangun aplikasi web secara end-to-end, mulai dari perancangan sistem hingga deployment.</p>
+                <br/>
+                <p>1. Halaman manajemen penyakit dan gejala</p><br/>
+                <img src="./img/portofolio/SPUM/penyakit.png" alt="Manajemen Penyakit" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                <img src="./img/portofolio/SPUM/gejala.png" alt="Manajemen Gejala" class="w-full max-w-[800px] h-auto mb-4 rounded"/><br/>
+                <p>2. Halaman training data</p><br/>
+                <img src="./img/portofolio/SPUM/training.png" alt="Training Data" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                <img src="./img/portofolio/SPUM/create-training.png" alt="Create Training Data" class="w-full max-w-[800px] h-auto mb-4 rounded"/><br/>
+                <p>3. Halaman diagnosis</p><br/>
+                <img src="./img/portofolio/SPUM/diagnosa.png" alt="Diagnosa" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                <img src="./img/portofolio/SPUM/hasil-diagnosa.png" alt="Hasil Diagnosa" class="w-full max-w-[800px] h-auto mb-4 rounded"/><br/>
+                `
+    },
+    {
+        id: 5,
+        title: "Snailhouse",
+        thumbnail: "./img/portofolio/snailhouse/Categories.png",
+        github: "http://github.com/wahyuyudi2801/warehouse-snailhouse",
+        website: "#",
+        description: "Snailhouse adalah aplikasi warehouse dan point of sales (POS) yang digunakan untuk mengelola stok barang dan transaksi penjualan di sebuah toko atau gudang.",
+        summary: `
+                <p>Snailhouse adalah aplikasi warehouse dan point of sales (POS) yang dirancang untuk membantu pengelolaan stok barang serta transaksi penjualan di toko maupun gudang. Proyek ini dikembangkan sebagai bagian dari pembelajaran mandiri untuk memperdalam pemahaman dalam pengembangan aplikasi berbasis web.</p>
+                <br/>
+                <p>Saya bertanggung jawab untuk merancang dan mengembangkan aplikasi ini secara keseluruhan. Teknologi yang digunakan meliputi Laravel, Inertia, React.js, Shadcn, serta MySQL sebagai database. Proses pengembangan dilakukan secara iteratif dengan pendekatan trial and error untuk menemukan solusi terbaik dalam implementasi fitur-fitur yang kompleks.</p>
+                <br/>
+                <p>Hasil dari proyek ini adalah sebuah aplikasi yang mampu mengelola stok barang, mencatat transaksi penjualan, dan menghasilkan laporan keuangan secara efektif. Selain itu, proyek ini memberikan pengalaman berharga dalam membangun aplikasi web secara menyeluruh, mulai dari perancangan hingga deployment, serta tau bagaimana cara memisahkan frontend dan backend.</p>
+                <br/>
+                <p>Berikut merupakan beberapa gambar dari proyek ini:</p><br/>
+                <p><b>Halaman Login</b></p>
+                <img src="./img/portofolio/snailhouse/login.png" alt="Login" class="w-full max-w-[800px] h-auto mb-4 rounded"/><br/>
+                <p><b>Halaman Overview</b></p>
+                <img src="./img/portofolio/snailhouse/Overview.png" alt="Overview" class="w-full max-w-[800px] h-auto mb-4 rounded"/><br/>
+                <p><b>Halaman manajemen kategori dan produk</b></p>
+                <img src="./img/portofolio/snailhouse/Categories.png" alt="Manajemen Kategori" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                <img src="./img/portofolio/snailhouse/Products.png" alt="Manajemen Produk" class="w-full max-w-[800px] h-auto mb-4 rounded"/><br/>
+                <p><b>Halaman Warehouse</b></p>
+                <img src="./img/portofolio/snailhouse/WH.png" alt="Warehouse" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                <img src="./img/portofolio/snailhouse/WH Detail.png" alt="Warehouse Detail" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                <p><b>Halaman Merchant</b></p>
+                <img src="./img/portofolio/snailhouse/Merchant.png" alt="Merchant" class="w-full max-w-[800px] h-auto mb-4 rounded"/><br/>
+                <img src="./img/portofolio/snailhouse/Merchant Detail.png" alt="Merchant Detail" class="w-full max-w-[800px] h-auto mb-4 rounded"/><br/>
+                <p><b>Halaman transaksi penjualan</b></p>
+                <img src="./img/portofolio/snailhouse/Transaction.png" alt="Transaksi Penjualan" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                <img src="./img/portofolio/snailhouse/Transaction Confirm.png" alt="Transaksi Penjualan Confirm" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                <img src="./img/portofolio/snailhouse/Transaction 1.png" alt="Transaksi Penjualan 1" class="w-full max-w-[800px] h-auto mb-4 rounded"/>
+                `
+    },
+]
+
+data.forEach(project => {
+    const projectElement = document.createElement('div');
+    projectElement.classList.add('w-full', 'sm:w-1/2', 'lg:w-1/3', 'p-4');
+    projectElement.innerHTML = `
+                <div class="bg-gray-100 dark:bg-dark-2 rounded-lg shadow-lg p-6">
+                    <h3 class="text-xl font-bold mb-2">${project.title}</h3>
+                    <img src="${project.thumbnail}" alt="${project.title}" class="w-full h-auto min-h-[200px] object-fill mb-4 rounded"/>
+                    <p class="text-sm text-gray-600 mb-4">${project.description}</p>
+                    <p class="text-right text-blue-600 font-bold cursor-pointer" onclick="openModal(${project.id})">Klik untuk melihat detail &raquo;</p>
+                </div>
+            `;
+    document.getElementById('portofolios').appendChild(projectElement);
+});
+
+function openModal(id) {
+    const project = data.find(p => p.id === id);
+    if (project) {
+        document.getElementById('title').textContent = project.title;
+        document.getElementById('thumbnail').src = project.thumbnail;
+        document.getElementById('githubLink').href = project.github;
+        document.getElementById('websiteLink').href = project.website;
+        document.getElementById('summary').innerHTML = project.summary;
+        document.getElementById('portfolioModal').classList.remove('hidden');
+    }
+}
+
+function closeModal() {
+    document.getElementById('portfolioModal').classList.add('hidden');
+}
--- src/script.js
+++ src/script.js
@@ -2,12 +2,12 @@ const skills = [
   {
     id: 1,
     name: "HTML",
-    url: "./img/skills/html.png",
+    url: "./img/skills/HTML.png",
   },
   {
     id: 2,
     name: "CSS",
-    url: "./img/skills/css.png",
+    url: "./img/skills/CSS.png",
   },
   {
     id: 3,
@@ -69,6 +69,36 @@ const skills = [
     name: "Typescript",
     url: "./img/skills/Typescript.png",
   },
+  {
+    id: 14,
+    name: "Node JS",
+    url: "./img/skills/nodejs.png",
+  },
+  {
+    id: 15,
+    name: "Docker",
+    url: "./img/skills/docker.png",
+  },
+  {
+    id: 16,
+    name: "PostgreSQL",
+    url: "./img/skills/postgres.png",
+  },
+  {
+    id: 17,
+    name: "Oracle",
+    url: "./img/skills/oracle.png",
+  },
+  {
+    id: 18,
+    name: "GitHub",
+    url: "./img/skills/github.png",
+  },
+  {
+    id: 19,
+    name: "GitLab",
+    url: "./img/skills/gitlab.png",
+  }
 ];
 
 let elementSkills = document.querySelector("#skills .skills");
@@ -87,65 +117,6 @@ skills.forEach((skill) => {
             class="h-20 w-full photos-skills"
         />
     </a>
-    `
-  );
-});
-
-// portfolio
-const dataPortfolios = [
-    {
-        name: "Aplikasi CAT (KUIS)",
-        img: "cat-app.jpg"
-    },
-    {
-        name: "Aplikasi Lelang Online",
-        img: "lelang-online.jpg"
-    },
-    {
-        name: "Aplikasi Referance Online",
-        img: "referance-app.jpg"
-    },
-    {
-        name: "Landing Page Schoot",
-        img: "schoot.jpg"
-    },
-    {
-        name: "Landing Page VISX",
-        img: "visx.jpg"
-    },
-    {
-        name: "Landing Page Qwords",
-        img: "qwords.png"
-    },
-];
-
-const elementPortfolio = document.querySelector("#portfolio .portfolio");
-dataPortfolios.forEach((portfolio) => {
-  elementPortfolio.insertAdjacentHTML(
-    "beforeend",
-    `
-        <div class="w-full px-4 md:w-1/2 xl:w-1/3">
-            <div class="relative mb-12">
-                <div class="overflow-hidden rounded-[10px]">
-                    <img
-                        src="./img/portofolio/${portfolio.img}"
-                        alt="${portfolio.name}"
-                        class="w-full h-[280px] object-cover"
-                    />
-                </div>
-                <div class="relative z-10 mx-7 -mt-20 rounded-lg bg-white dark:bg-dark-2 py-[34px] px-3 text-center shadow-portfolio dark:shadow-box-dark">
-                    <h3 class="text-dark dark:text-white mb-5 text-xl font-bold">
-                        ${portfolio.name}
-                    </h3>
-                    <a
-                        href="javascript:void(0)"
-                        class="text-body-color dark:text-dark-6 hover:border-primary hover:bg-primary inline-block rounded-md border border-stroke dark:border-dark-3 py-[10px] px-7 text-sm font-medium transition hover:text-white"
-                    >
-                        Lihat Detail
-                    </a>
-                </div>
-            </div>
-        </div>
     `
   );
 });
